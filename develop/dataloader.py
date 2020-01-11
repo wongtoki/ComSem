53c4f3313104e100ddf0e1b00ba907c56d8858e4
wongtoki5@gmail.com <wongtoki5@gmail.com>
2020-01-11 12:42:17 +0100

    Added SVM  model

--- develop/dataloader.py
+++ develop/dataloader.py
@@ -5,7 +5,7 @@ class Loader:
     @staticmethod
     def load_data(path: str):
         """The path should a txt file containing sick data"""
-
+        print("Loading data...")
         data = {}
 
         with open(path, 'r') as file:
@@ -23,10 +23,16 @@ class Loader:
 
                 i += 1
 
+        data["tokens"] = []
+        for x, sent in enumerate(data["sentence_A"]):
+            tokens = str(sent) + " " + data["sentence_B"][x]
+            tokens = tokens.split()
+            data["tokens"].append(tokens)
+
         return pd.DataFrame(data)
 
 
 if __name__ == "__main__":
     path = "../NLI2FOLI/SICK/SICK_train.txt"
     data = Loader.load_data(path)
-    print(data)
+    print(data["tokens"][0])
--- develop/main.py
+++ develop/main.py
@@ -1,16 +1,39 @@
+from sklearn.svm import SVC, LinearSVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
 from dataloader import Loader
+from sklearn.metrics import *
 
 
 class Model:
     def __init__(self, data):
         self.train, self.test = train_test_split(data, test_size=0.2)
-        print(len(data))
-        print(len(self.test))
         pass
+
+    def train_model(self):
+        """Using a TFIDF vectorisor"""
+        xtrain = self.train["tokens"]
+        ytrain = self.train["entailment_judgment"]
+
+        vec = TfidfVectorizer(preprocessor=lambda x: x, tokenizer=lambda x: x)
+        classifier = Pipeline([('vec', vec), ('cls', LinearSVC(C=1))])
+        classifier.fit(xtrain, ytrain)
+        self.model = classifier
+
+    def test_model(self):
+        xtest = self.test["tokens"]
+        ytest = self.test["entailment_judgment"]
+
+        if self.model != None:
+            pred = self.model.predict(xtest)
+            print(classification_report(ytest, pred))
+        else:
+            print("Model not trained.")
 
 
 if __name__ == "__main__":
     model = Model(Loader.load_data("../NLI2FOLI/SICK/SICK_train.txt"))
+    model.train_model()
+    model.test_model()
